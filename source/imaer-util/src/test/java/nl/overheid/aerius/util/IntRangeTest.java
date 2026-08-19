a810663200dd74a07c9164fa3f78eddf4bd494f7
Michiel Janssen <78349449+MichielJanssen-DAT@users.noreply.github.com>
2026-08-19 16:27:35 +0200

    AER-4530 Update IntRange toString (#396)
    Updated to scientific notation that comes overeen with stageclass texts. Examples (taken from the updated tests):
    - `<20` -> `< 20`
    - `>10 - <=20` -> `(10-20]`
    - `>=10` -> `≥ 10`

--- source/imaer-util/src/test/java/nl/overheid/aerius/util/IntRangeTest.java
+++ source/imaer-util/src/test/java/nl/overheid/aerius/util/IntRangeTest.java
@@ -87,13 +87,13 @@ class IntRangeTest {
 
   static List<Arguments> toStringData() {
     return List.of(
-        Arguments.of("[,20)", "<20"),
-        Arguments.of("[,20]", "<=20"),
-        Arguments.of("[10,20]", ">=10 - <=20"),
-        Arguments.of("(10,20]", ">10 - <=20"),
-        Arguments.of("(10,20)", ">10 - <20"),
-        Arguments.of("[10,20)", ">=10 - <20"),
-        Arguments.of("[10,)", ">=10"),
-        Arguments.of("(10,)", ">10"));
+        Arguments.of("[,20)", "< 20"),
+        Arguments.of("[,20]", "≤ 20"),
+        Arguments.of("[10,20]", "[10-20]"),
+        Arguments.of("(10,20]", "(10-20]"),
+        Arguments.of("(10,20)", "(10-20)"),
+        Arguments.of("[10,20)", "[10-20)"),
+        Arguments.of("[10,)", "≥ 10"),
+        Arguments.of("(10,)", "> 10"));
   }
 }
